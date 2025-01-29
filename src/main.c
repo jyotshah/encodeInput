3bec34bf21749c4f8c363540f210edbe922c093a
Jyot Shah <jshah1717@conestogac.on.ca>
2025-01-29 12:12:25 -0500

    basics

--- src/main.c
+++ src/main.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+/*
+Authors: Jyot Shah, Soham and Ashwini
+Assignment: Assignment 2 - Linux Utility
+Date: 29-01-2025
+File: main.c
+*/
 
-int main(void) {
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "srecord.h"
+#include "assembly.h"
+#include "file_io.h"
+
+void processArguments(int argc, char **argv, char *inputFile, char *outputFile, int *isSRecordFormat, int *help);
+
+int main(int argc, char **argv) {
+    char inputFile[MAX_FILENAME_LENGTH] = {0};
+    char outputFile[MAX_FILENAME_LENGTH] = {0};
+    int isSRecordFormat = 0, help = 0;
+
+    processArguments(argc, argv, inputFile, outputFile, &isSRecordFormat, &help);
+
+
     return 0;
 }
--- src/srecord.c
+++ src/srecord.c
@@ -1,5 +1,15 @@
 //
 // Created by jyotshah on 29/01/25.
 //
-
 #include "srecord.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static void byteToHex(unsigned char byte, char *hex) {
+    const char *hexDigits = "0123456789ABCDEF";
+    hex[0] = hexDigits[(byte >> 4) & 0x0F];
+    hex[1] = hexDigits[byte & 0x0F];
+}
+
+
